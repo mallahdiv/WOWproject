4f384ac75dd0c564647eb2b1a09e540e0b0431ac
Darron McIntyre <dmcin003@fiu.edu>
2020-12-07 17:23:58 -0500

    added some imports for flask, our app route for our home page with search bar, and templates directory with home.html file

--- BaseCode/wow_project_code.py
+++ BaseCode/wow_project_code.py
@@ -3,3 +3,17 @@
 #                                      By Kevin Harper, Darron McIntyre, Mallah-Divine Mallah
 
 
+from flask import Flask, request,render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/',methods=["GET","POST"])
+def home():
+    return render_template('home.html')
+
+
+# this allows the python file to run
+if __name__ == "__main__":
+    app.run()
