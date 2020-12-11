962b88ee59785b0d6c43ea4c90df1d4f7586934e
dmcin003 <dmcin003@fiu.edu>
2020-12-10 20:24:11 -0500

    added sql like functionality to search feature

--- BaseCode/wow_project_code.py
+++ BaseCode/wow_project_code.py
@@ -44,10 +44,11 @@ def home():
     last_name = ' '
     if request.form:
         last_name = request.form.get("officer")
-    last_name = (str(last_name))
+        
+    last_name = str(last_name)
 
     
-    officers = db.session.query(allegations).filter(allegations.last_name == last_name.capitalize())
+    officers = db.session.query(allegations).filter(allegations.last_name.like(last_name.capitalize() + "%"))
     for officer in officers:
         print(officer.first_name, officer.last_name, officer.shield_no)
     
