a713288007db90b3c579eb9472518aa0c3efd721
dmcin003 <dmcin003@fiu.edu>
2020-12-11 13:08:08 -0500

    added search by badge # code

--- BaseCode/wow_project_code.py
+++ BaseCode/wow_project_code.py
@@ -41,16 +41,23 @@ allegations = Base.classes.allegations
 
 @app.route('/',methods=["GET","POST"])
 def home():
-    last_name = ' '
+    search_input = ' '
     if request.form:
-        last_name = request.form.get("officer")
+        search_input = request.form.get("officer")
         
-    last_name = str(last_name)
-
     
-    officers = db.session.query(allegations).filter(allegations.last_name.like(last_name.capitalize() + "%"))
-    for officer in officers:
-        print(officer.first_name, officer.last_name, officer.shield_no)
+    
+    search_input = str(search_input)
+    if search_input.isdigit():
+        #search by badge number
+        officers = db.session.query(allegations).filter(allegations.shield_no == search_input)
+        for officer in officers:
+            print(officer.first_name, officer.last_name, officer.shield_no)
+    else:
+        #search by officer last name
+        officers = db.session.query(allegations).filter(allegations.last_name.like(search_input.capitalize() + "%"))
+        for officer in officers:
+            print(officer.first_name, officer.last_name, officer.shield_no)
     
     return render_template('search.html')
 
