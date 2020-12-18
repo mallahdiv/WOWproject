3a8ec7652cc018bc224e96f8ab615a06611d5196
kevinharper7 <kevinharper8@protonmail.com>
2020-12-18 16:07:04 -0500

    Changed Design

--- BaseCode/application.py
+++ BaseCode/application.py
@@ -21,7 +21,7 @@ from sqlalchemy.ext.automap import automap_base
 app = Flask(__name__)
 app.secret_key = 'wow'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:wowproject@justiceleague.cr2qomviop3w.us-east-2.rds.amazonaws.com/postgres' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://justiceleague:wowproject@localhost/officerDB'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
